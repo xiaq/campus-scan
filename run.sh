f0c0ea6143efb5880e1f17dc762da5ed2408f993
Cheer Xiao <xiaqqaix@gmail.com>
2012-06-05 22:08:15 +0800

    more reasonable run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
-bindir="$(dirname "$0")"
-destdir="$(date +%Y%m%d)"
-if [ -f "$destdir" ]; then
-    echo "$destdir already exists"
-    exit 1
+bindir=$(dirname "$(readlink -f "$0")")
+do_scan=no
+do_plot=no
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+    -s)
+        do_scan=yes ;;
+    -p)
+        do_plot=yes ;;
+    *)
+        dest_dir=$1 ;;
+    esac
+    shift
+done
+
+if [ -z "$dest_dir" ]; then
+    dest_dir="$(date +%Y%m%d)"
+    if [ -f "$dest_dir" ]; then
+        echo "$dest_dir already exists"
+        exit 1
+    fi
 fi
-mkdir "$destdir"
-cd "$destdir"
-ln -s "$bindir"/web/* .
+
+mkdir "$dest_dir"
+cd "$dest_dir"
+
+[ "$do_plot" = yes ] && ln "$bindir"/web/* .
 
 for sub in uphosts openports; do
-    "$bindir"/main.py $sub scan > $sub.json
-    "$bindir"/main.py $sub plot < $sub.json
+    [ "$do_scan" = yes ] && "$bindir"/main.py $sub scan -f $sub.json
+    [ "$do_plot" = yes ] && "$bindir"/main.py $sub plot -f $sub.json
 done
 
