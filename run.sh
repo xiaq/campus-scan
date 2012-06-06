02835bb65bf259995dafc2897674992d2ebc8ec8
Cheer Xiao <xiaqqaix@gmail.com>
2012-06-06 19:34:11 +0800

    run.sh: better argument parsing with getopt

--- run.sh
+++ run.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
+
+ln_or_cp() {
+    ln "$@" || cp "$@"
+}
+
+on_exit() {
+    STATUS=$?
+    rm "$lock_file_f"
+    exit "$STATUS"
+}
+
 bindir=$(dirname "$(readlink -f "$0")")
 do_scan=no
 do_plot=no
+set_verbose=no
+
+TEMP=$(getopt -o spv -- "$@")
+if [ $? != 0 ]; then
+    echo 'Terminating...'
+    exit 1
+fi
+
+eval set -- "$TEMP"
 
 while [ "$#" -gt 0 ]; do
     case "$1" in
@@ -9,15 +29,33 @@ while [ "$#" -gt 0 ]; do
         do_scan=yes ;;
     -p)
         do_plot=yes ;;
-    *)
-        dest_dir=$1 ;;
+    -v)
+        set_verbose=yes ;;
+    --)
+        shift
+        break ;;
     esac
     shift
 done
 
+dest_dir="$1"
+
+[ "$set_verbose" = yes ] && set -v
+
+lock_file=campus-scan.lock
+
+if [ -e "$lock_file" ]; then
+    echo 'Lock exists, exiting...'
+    exit 1
+else
+    touch "$lock_file"
+    lock_file_f="$(readlink -f "$lock_file")"
+    trap on_exit INT QUIT TERM
+fi
+
 if [ -z "$dest_dir" ]; then
     dest_dir="$(date +%Y%m%d)"
-    if [ -f "$dest_dir" ]; then
+    if [ -e "$dest_dir" ]; then
         echo "$dest_dir already exists"
         exit 1
     fi
@@ -26,7 +64,7 @@ fi
 mkdir "$dest_dir"
 cd "$dest_dir"
 
-[ "$do_plot" = yes ] && ln "$bindir"/web/* .
+[ "$do_plot" = yes ] && ln_or_cp "$bindir"/web/* .
 
 for sub in uphosts openports; do
     [ "$do_scan" = yes ] && "$bindir"/main.py $sub scan -f $sub.json
